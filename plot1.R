e39fb1d8ca7b60e10b4d2582f9d0fc740475a96f
=Adam Davidson <=shinigami.adam@gmail.com>
2014-07-13 20:05:45 +0100

    updated plot1 script/png

--- plot1.R
+++ plot1.R
@@ -4,9 +4,9 @@ plot1 <- function(dataFile = "data/household_power_consumption.txt"){
    if(!file.exists(zipFile)){
      download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile=zipFile)
    }
-   unzip(zipFile, exdir="data")
+   unzip(zipFile, exdir="data") 
   }
-  message("Creating data frame")
+  message("Creating data frame") 
   household <- read.table(dataFile, header = TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
   household_sub <- household[(as.Date(household$Date, format="%d/%m/%Y") == "2007-02-01" | as.Date(household$Date, format="%d/%m/%Y") == "2007-02-02"),]
   household_sub <- cbind(as.POSIXct(paste(household_sub$Date, " ",household_sub$Time), format="%d/%m/%Y %H:%M:%S"),household_sub)
@@ -14,7 +14,7 @@ plot1 <- function(dataFile = "data/household_power_consumption.txt"){
   message("Creating plot...")
   png("plot1.png")
   #Set device to png 
-  with(household_sub, hist(Global_active_power, col="red", xlab="Global Active Power (kilowatts)"))
+  with(household_sub, hist(Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power"))
   title("Global Active Power")
   #create png file
   dev.off() 
